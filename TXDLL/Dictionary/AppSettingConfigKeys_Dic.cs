ab93c32584c8c14343ee5bf15d998890033ac011
XinTan <838524739@qq.com>
2017-11-13 15:31:39 +0800

    add AppSettingConfigKeys_Dic

--- TXDLL/Dictionary/AppSettingConfigKeys_Dic.cs
+++ TXDLL/Dictionary/AppSettingConfigKeys_Dic.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace TXDLL.Dictionary
 {
+    /// <summary>
+    /// 在appsetting里面可以配置的项的key
+    /// </summary>
     public class AppSettingConfigKeys_Dic
     {
-        
+        /// <summary>
+        /// 数据库类型 数字 Oracle=1,SqlServer=2,MySQL=3,Access=4
+        /// </summary>
+        public const string DatabaseType = "DatabaseType";
+        /// <summary>
+        /// 默认的数据库连接字符串
+        /// </summary>
+        public const string DefaultConnStr = "DefaultConnStr";
+        /// <summary>
+        /// 连接超时时间。单位毫秒
+        /// </summary>
+        public const string TimeOut = "TimeOut";
     }
 }
